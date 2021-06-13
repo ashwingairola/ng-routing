d721acbfead7590f868b03a256c816f7058ef274
Ashwin <ashwingairola999@gmail.com>
2021-06-13 21:21:26 +0530

    126. Setting Up and Loading Routes + 127. Navigating with Router Links

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+
+const routes: Route[] = [
+	{ path: 'users', component: UsersComponent },
+	{ path: 'servers', component: ServersComponent },
+	{ path: '', component: HomeComponent }
+];
+
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -11,22 +10,20 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [ServersService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		UsersComponent,
+		ServersComponent,
+		UserComponent,
+		EditServerComponent,
+		ServerComponent
+	],
+	imports: [BrowserModule, FormsModule, AppRoutingModule],
+	providers: [ServersService],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
